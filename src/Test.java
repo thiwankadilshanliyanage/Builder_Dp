36caa6f6056ca57f6c6f170c28dec07152d0c897
thiwankadilshanliyanage <79803162+thiwankadilshanliyanage@users.noreply.github.com>
2021-10-28 13:24:40 +0530

    first commit

--- src/Test.java
+++ src/Test.java
@@ -10,11 +10,99 @@
  */
 public class Test {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+      
+        User u = new User(1,"Shan","shan@gmail.com","123","Colombo");
+       
     }
     
+}
+class User{
+
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String city;
+
+    public User(int id, String name, String email, String password, String city) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.city = city;
+    }
+    
+    
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
